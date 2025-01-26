e561fe917087b66da1eff1ecc004c74696869087
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-26 06:22:12 +0000

    Cleanup

--- mafiarp/schema/schema.lua
+++ mafiarp/schema/schema.lua
@@ -1,9 +1,4 @@
-﻿--[[--
-    Schema configuration options.
-
-    This is meant to define the Schema's default information.
-]]
-SCHEMA.name = "MafiaRP"
+﻿SCHEMA.name = "MafiaRP"
 SCHEMA.author = "76561198312513285"
 SCHEMA.discord = "@liliaplayer"
 SCHEMA.desc = "A MafiaRP Schema"
